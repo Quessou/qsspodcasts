0ba0747966b60bdd4926a3a812f7478d9fa99bab
Maxime <maximemikotajewski@gmail.com>
2022-10-24 19:01:13 +0200

    Now sleeps between polls

--- frontend/src/crossterm_async_event.rs
+++ frontend/src/crossterm_async_event.rs
@@ -4,7 +4,7 @@ use std::pin::Pin;
 use std::task::{Context, Poll};
 use std::time::Duration;
 
-use tokio::time::timeout;
+use tokio::time::{sleep, timeout};
 
 // TODO : find more relevant names when I'll understand what I'm doing
 struct Polled {}
@@ -30,8 +30,18 @@ impl Future for Polled {
 }
 
 pub async fn poll(d: Duration) -> Result<bool, ()> {
-    let polled = Polled::new();
-    match timeout(d, polled).await {
+    let mdr = async {
+        loop {
+            let a = crossterm_poll(Duration::from_secs(0));
+            if a.is_ok() && a.unwrap() {
+                break;
+            }
+            sleep(Duration::from_millis(50)).await;
+        }
+        true
+    };
+
+    match timeout(d, mdr).await {
         Ok(b) => Ok(b),
         Err(_) => Err(()),
     }
